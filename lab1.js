cd9031f15d879f0dd6a0e25f29dacaf5bb1367a6
Md-Tanvir-Bin-Zoha <21-45307-2@student.aiub.edu>
2025-04-29 03:05:21 +0600

    initcom

---
+++ lab1.js
@@ -0,0 +1,118 @@
+/*
+//default parameter
+//const a=(name="abd")=>`Hello ${name}`
+
+//console.log(a("cd"));
+
+//creating class (constractor)
+class b{
+    constructor(name)
+    {
+        this.name=name
+    }
+    sayHello()
+    {
+        setTimeout(() => {
+            console.log(this.name)
+            
+        }, 1000);
+    }
+}
+const ab=new b("Tanvir")
+console.log(ab.name);
+ab.sayHello()
+
+//call back function (after finishing its parents work)
+//default parameter
+//static
+
+//loop
+//for of
+const a=[2,3,4]
+
+for(let ab of a){
+    console.log(ab);
+}
+/
+const b=[2,3,4]
+//for in
+for(let ab in b){
+    console.log(b[ab]);
+}
+    
+//for each
+
+const a=[2,3,4]
+a.forEach((el,i,array)=>{
+    console.log(array);
+})
+*/
+/*
+
+
+//Data Type, Object
+const student =
+{
+    name : "Tanvir",
+    age : 23,
+    class : 13
+
+}
+console.log(student.age);*/
+/*
+ b = null
+ console.log(typeof b)*/
+
+/*
+ // Operators 
+ //arithmetic
+ let a=5;
+ let b=5;
+ console.log(a+b);
+ console.log(a-b);
+ console.log(a*b);
+ console.log(a/b);
+ console.log(a%b);
+ console.log(a**b); */
+
+ //unary
+// let a = 5;
+// let b="5";
+// console.log(a==b);
+
+// let age=20
+// if (age>=18){
+//     console.log("You are joss")
+// } else {
+//     console.log("You are not joss")
+// }
+
+// let num =11;
+// if (num%2 === 0){
+//     console.log("Even")
+// }else{
+//     console.log("ODD")
+// }
+// let age = 65
+// if (age<18) {
+//     console.log("Junior")
+// } else if(age>60) {
+//     console.log("Senior")
+// } else{
+//     console.log("Adult")
+// }
+// for (let write =1; write<=100; write++){
+//     console.log("Tanvir")
+// }
+// let i=1;
+// while(i<5){
+//     console.log("Hasin")
+//     i++;
+// }
+
+const a="Tanvir"
+
+for(let ab of a){
+    console.log(ab);
+}
+
